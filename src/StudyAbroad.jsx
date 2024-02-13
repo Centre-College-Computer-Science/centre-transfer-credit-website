5486a236d2223cff871cd6458d170635946aaf1e
Jackson Arnold <jackson.n.arnold@gmail.com>
2024-02-13 14:21:14 -0500

    Imported existing code

---
+++ src/StudyAbroad.jsx
@@ -0,0 +1,26 @@
+export default function StudyAbroad() {
+  return (
+    <div className="App">
+      <div class="container">
+        <div class="container">
+          <h2>Study Abroad Transfer Credit Policy</h2>
+        </div>
+        <div class="container">
+          <p>
+            If you are a current Centre student aiming to transfer over a study
+            abroad credit, then please consult the study abroad page.
+          </p>
+        </div>
+        <div class="container">
+          <h3>Approved Study Abroad Credits</h3>
+        </div>
+        <div class="container">
+          <p>
+            This table will be populated by the registrar, and is not universal
+            across all of Centre College's study abroad programs.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
