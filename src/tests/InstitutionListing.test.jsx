738d8a5ef7b85e7f769933119bfe5476f542ce1b
emfroa <emzhuang2763@gmail.com>
2024-02-20 18:47:05 -0500

    Quick commit

--- src/tests/InstitutionListing.test.jsx
+++ src/tests/InstitutionListing.test.jsx
@@ -1,15 +1,36 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from 'vitest';
 import * as React from "react";
-import { render, screen } from "@testing-library/react";
-import InstitutionListing from './InstitutionListing.jsx';
+import { render, screen, fireEvent } from "@testing-library/react";
+import InstitutionListing from '../InstitutionListing.jsx';
+import { LevelContext } from '../LevelContext'; 
 
 
 it("render", ()=>{
-   const institutionName = "BERRY COLLEGE"
+   const mockToggleSelected = vi.fn(); //mock function
 
+   //a mockup list of a course
+   const courseDetails = {
+      rewarding_institution: 'Institution Name',
+      ri_courseTitle: 'RI Course Title',
+      centre_courseTitle: 'Centre Course Title',
+      centre_course_credits: '3',
+      checked: false
+    };
 
-   render(<InstitutionListing />)
+    //setting up the InstitutionListing by rendering it
 
+render(
+   <LevelContext.Provider value={{ setCurrentInstitution: mockToggleSelected }}>
+     <InstitutionListing institution={courseDetails.rewarding_institution} />
+   </LevelContext.Provider>
+ )
+   // debug();
+   expect(screen.getByText(courseDetails.rewarding_institution)).toBeInTheDocument();
 
-   expect(screen.getByText(institutionName));
+   const nameFieldElement = screen.getByText(courseDetails.rewarding_institution); //name of the course
+   fireEvent.click(nameFieldElement); //clicks on the name
+
+   //see if its called?
+   //see if the text appear?
+   expect(mockToggleSelected).toHaveBeenCalledWith(courseDetails.rewarding_institution);
 });
