5382892c1f9bf81ca546295a261ba82c319dc203
JoannaMcGill <joanna.mcgill@centre.edu>
2024-02-27 18:06:59 -0500

    more tests

--- src/tests/CourseTable.test.jsx
+++ src/tests/CourseTable.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import CourseTable from "../CourseTable";
+import { fireEvent } from "@testing-library/react";
 
 describe("CourseTable Component", () => {
   it("renders without crashing", () => {
