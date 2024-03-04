fd266f010e03bed47f240613ac5df4c798e997df
Diego-Sic <diego.sic@centre.edu>
2024-03-04 08:33:46 -0500

    Last changes in App.test.jsx

--- src/tests/util.test.jsx
+++ src/tests/util.test.jsx
@@ -1,13 +1,15 @@
 // utils.test.ts
-import { describe, it, expect } from 'vitest';
-import { makeTitleCase } from '../utils/utils';
+import { describe, it, expect } from "vitest";
+import { makeTitleCase } from "../utils/utils";
 
-describe('makeTitleCase', () => {
+describe("makeTitleCase", () => {
   it('converts simple strings to title case, excluding "of"', () => {
-    expect(makeTitleCase('university of hard knocks')).toBe('University of Hard Knocks');
+    expect(makeTitleCase("university of hard knocks")).toBe(
+      "University of Hard Knocks"
+    );
   });
 
-  it('works with empty strings', () => {
-    expect(makeTitleCase('')).toBe('');
+  it("works with empty strings", () => {
+    expect(makeTitleCase("")).toBe("");
   });
 });
