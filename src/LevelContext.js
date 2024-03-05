d808a62f28a51f590d91f4c90616a0908ea8dca5
districtjackson <districtjackson@users.noreply.github.com>
2024-03-05 21:47:51 +0000

    deploy: af700888287ef034318524d6030c6277a389ab13

--- src/LevelContext.js
+++
@@ -1,5 +0,0 @@
-//ensures that information across pages will be saved, ensures that there is no need for prop drilling 
-//acts largely as a global variable
-import { createContext } from "react";
-
-export const LevelContext = createContext(1);
--- src/utils/utils.js
+++
@@ -1,7 +0,0 @@
-// utils.js
-
-// We might have to add some sort of way to handle different things and special characters
-export const makeTitleCase = (string) => {
-    return string.split(/\s+/).map(word => word === 'of' ? word : word.charAt(0).toUpperCase() + word.substring(1).toLowerCase()).join(' ');
-  };
-  
