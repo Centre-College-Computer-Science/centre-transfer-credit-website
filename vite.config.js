f3fe9a4dcdceaf560d7f662e589238031ef194ff
Jackson Arnold <jackson.n.arnold@gmail.com>
2024-03-07 12:38:57 -0500

    Changed base
    Oops I did it again

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
   
   // https://vitejs.dev/config/
   export default defineConfig({
-      base: './',
+      base: '/centre-transfer-credit-website/',
       plugins: [react()],
       assetsInclude: "**/*.csv",
       test: {
