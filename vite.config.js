739831d9ee227ad1aecf28a5601fc9066fbf91ed
Jackson Arnold <jackson.n.arnold@gmail.com>
2024-03-07 13:17:50 -0500

    Change base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
   
   // https://vitejs.dev/config/
   export default defineConfig({
-      base: '/centre-transfer-credit-website/',
+      //base: '/centre-transfer-credit-website/',
       plugins: [react()],
       assetsInclude: "**/*.csv",
       test: {
