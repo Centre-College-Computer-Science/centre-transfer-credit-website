acf28a090b2132578e80a9d175ab934ed2b72bf7
Odile Clements <molly.clements@centre.edu>
2024-04-16 14:07:37 -0400

    Formatting new text

--- src/App.jsx
+++ src/App.jsx
@@ -50,12 +50,26 @@ export default function App() {
     >
       <div className="container  parent_sticky">
         <div className="parent_content"> 
-        <p>
-          This tool helps transferring students to determine what classes can be transferred in to Centre.
+        <p className="tab">&emsp;&emsp;Centre’s policies in these areas are designed to reward extraordinary achievement while at the same time supporting our belief that the best Centre College experience is a four-year experience. 
+          It is our philosophy that everything a student does prior to high school graduation is preparation for college, and all Centre students enter with exceptional academic credentials. 
+          Some of those credentials include college course work and others do not. Consequently, we limit the amount of credit first-year students can be awarded prior to their enrollment
+           at the College:<br /></p>
+        <div className="tabbedMargin">
+        <p>1.    A maximum of 24 hours of pre-matriculation credits may be awarded to first-year students from all sources (e.g., Advanced Placement, International Baccalaureate, dual-credit, all other college credit).<br />
+
+        2.    Credit will not be awarded for any course or examination completed prior to the start of the junior year in high school.<br />
+
+        3.    Credit will not be awarded for any course or examination that serves to satisfy the College’s entrance requirements.<br />
+
+        4.    Students who legitimately exceed the 24-hour limit may choose which credits will be awarded, and may adjust his or her choices later subject to the constraints of any other College policies.<br />
+
+        5.    Regardless of credit granted, students must complete at least one general education course in residence in each of the following three exploration areas: arts & humanities, social studies, science & mathematics.<br /></p>
+        </div><p>
+          {/* This tool helps transferring students to determine what classes can be transferred in to Centre. */}
         </p>
         <p>
-          Information on Centre's pre-matriculation credit policy, including <b>AP</b>, <b>IB</b>, CLEP, and international exams, can be found 
-          <a href="https://www.centre.edu/apply/college-credit-equivalency">here</a>.
+          {/* Information on Centre's pre-matriculation credit policy, including <b>AP</b>, <b>IB</b>, CLEP, and international exams, can be found  */}
+          {/* <a href="https://www.centre.edu/apply/college-credit-equivalency">here</a>. */}
         </p>
 
         <p>
--- src/TransferCredits.jsx
+++ src/TransferCredits.jsx
@@ -69,10 +69,13 @@ export default function TransferCredits() {
       <div className="sticky">
       <div class={accorChange ? "accordion" : "accordion--open"} data-accordion-open-text="Click to Open" data-accordion-close-text="Click to Close">
 
-        <button class="accordion__button btn sticky_space" onClick={clicked} aria-expanded =  {accorChange}>
+        <button class="accordion__button btn sticky_space" onClick={clicked} aria-expanded =  {accorChange}  style={accorChange ? {'background-color': "white"} : {'background-color':"#ffcd00"}}>
                 <span class="show-for-sr"></span>
                 <span class="accordion__button-text"><b> Saved Courses</b><span className="fix_count_size">{creditTotal == 0 ? null : " (" + creditTotal + ")"}</span></span>
-                <svg class="accordion__icon" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 30 30"><path class="accordion__icon-path accordion__icon-path--horizontal" d="M27.5 17.4h-25C1.2 17.4.1 16.3.1 15s1.1-2.4 2.4-2.4h25c1.3 0 2.4 1.1 2.4 2.4s-1.1 2.4-2.4 2.4z"/><path class="accordion__icon-path accordion__icon-path--vertical" d="M14.5 29.9c-1.3 0-2.4-1.1-2.4-2.4v-25c0-1.3 1.1-2.4 2.4-2.4s2.4 1.1 2.4 2.4v25c0 1.3-1.1 2.4-2.4 2.4z"/></svg>
+                <svg class="accordion__icon" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 30 30">
+                  <path class="accordion__icon-path accordion__icon-path--horizontal" d="M27.5 17.4h-25C1.2 17.4.1 16.3.1 15s1.1-2.4 2.4-2.4h25c1.3 0 2.4 1.1 2.4 2.4s-1.1 2.4-2.4 2.4z"/>
+                  <path class="accordion__icon-path accordion__icon-path--vertical" d="M14.5 29.9c-1.3 0-2.4-1.1-2.4-2.4v-25c0-1.3 1.1-2.4 2.4-2.4s2.4 1.1 2.4 2.4v25c0 1.3-1.1 2.4-2.4 2.4z"></path>
+                  </svg>
         </button>
         <div class="accordion__content wysiwyg backgroundFormatting" style={accorChange ? {display:"block", transitionTimingFunction : "ease-in-out"} : {display:"none"}}>
         
